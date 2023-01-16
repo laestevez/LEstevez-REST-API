db07008bada74e495f641759a5e14210b042bfd7
laestevez <luisangel.e10@gmail.com>
2023-01-16 10:22:35 -0800

    added getting user by id

--- backend.js
+++ backend.js
@@ -4,17 +4,21 @@ const port = 5000;
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByName(name);
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
         result = {users_list: result};
         res.send(result);
     }
-    else{
-        res.send(users);
-    }
 });
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
