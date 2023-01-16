fe52c28e3b818960d4f041cc864d096fcfb32927
laestevez <luisangel.e10@gmail.com>
2023-01-16 11:41:45 -0800

    added Delete functionality

--- backend.js
+++ backend.js
@@ -4,7 +4,23 @@ const port = 5000;
 
 app.use(express.json());
 
-app.get('/users/:id', (req, res) => {
+app.get('/users', (req, res) => { //get users
+    res.send(users);
+});
+
+app.get('/users', (req, res) => { //get users by name
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+app.get('/users/:id', (req, res) => { //get users by id
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
     if (result === undefined || result.length == 0)
@@ -25,8 +41,26 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+app.delete('/users/:id', (req, res) => { //delete users by id
+    const id = req.params['id'];
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+
+    if(userDeleteById(id))
+        res.status(204).send('Resource Deleted.');
+});
+
 function addUser(user){
     users['users_list'].push(user);
+}
+
+function userDeleteById(id) {
+    const idx = users['users_list'].findIndex( (user) => user['id'] === id);
+    if(idx == -1)
+        return false;
+    delete users.users_list.splice(idx,1);
+    return true;
 }
 
 function findUserById(id) {
